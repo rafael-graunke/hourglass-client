d270b8df550339150c1e0532901adfe02e4a274f
Rafael Graunke <rafael.graunke@yahoo.com>
2022-04-23 00:01:04 -0300

    Alterada ordem itens menu

--- src/components/layout/Sidebar/index.js
+++ src/components/layout/Sidebar/index.js
@@ -4,8 +4,8 @@ import styles from './styles.module.css';
 function Sidebar() {
   return (
     <div className={styles.panel}>
-      <MenuItem to="/relatorios" texto="Relatórios" />
       <MenuItem to="/horas" texto="Horas" />
+      <MenuItem to="/relatorios" texto="Relatórios" />
       <MenuItem to="/emails" texto="E-mails" />
     </div>
   );
